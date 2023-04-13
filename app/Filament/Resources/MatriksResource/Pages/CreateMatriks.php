6615cd41bc4ca4113caca60aea2d4ac7498a5cf3
widura26 <widurategalrejo@gmail.com>
2023-04-13 20:07:31 +0700

    first commit

---
+++ app/Filament/Resources/MatriksResource/Pages/CreateMatriks.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\MatriksResource\Pages;
+
+use App\Filament\Resources\MatriksResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMatriks extends CreateRecord
+{
+    protected static string $resource = MatriksResource::class;
+}
---
+++ database/migrations/2023_04_02_033804_create_indikator_keberhasilans_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('indikator_keberhasilans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('matriks_id')->constrained()->cascadeOnDelete();
+            $table->text('teks_indikator');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('indikator_keberhasilans');
+    }
+};
